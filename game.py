8330aa399e2f6012d5932457509490c2735f9820
amydiaz1 <amydiaz1@mail.weber.edu>
2025-01-16 10:08:16 -0700

    ADD: Docstring

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
+'''
+Adventure Game
+Author: Amy DIaz
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices to navigate through a mysterious forest.
+
+'''
+
+
+#Welcome message and introducton
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
